0d9d30676d60aaac27197e4445677506fd22e58e
CooLoongWu <742420210@qq.com>
2017-08-09 16:53:25 +0800

    1、实现自动规划路程； 2、实现小车平滑移动；

--- app/src/main/java/com/cooloongwu/minitraffic/MainActivity.java
+++ app/src/main/java/com/cooloongwu/minitraffic/MainActivity.java
@@ -3,8 +3,10 @@ package com.cooloongwu.minitraffic;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,7 +14,6 @@ import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
-import com.amap.api.maps.model.BitmapDescriptor;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
@@ -21,18 +22,29 @@ import com.amap.api.maps.model.Polyline;
 import com.amap.api.maps.model.PolylineOptions;
 import com.amap.api.maps.utils.SpatialRelationUtil;
 import com.amap.api.maps.utils.overlay.SmoothMoveMarker;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.route.BusRouteResult;
+import com.amap.api.services.route.DrivePath;
+import com.amap.api.services.route.DriveRouteResult;
+import com.amap.api.services.route.DriveStep;
+import com.amap.api.services.route.RideRouteResult;
+import com.amap.api.services.route.RouteSearch;
+import com.amap.api.services.route.WalkRouteResult;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RouteSearch.OnRouteSearchListener {
 
     private MapView mapView = null;
+    private Button btn, plan;
     private AMap aMap = null;
-    private UiSettings uiSettings = null;
 
     private Polyline mPolyline;
+
+    //轨迹点
+    List<LatLng> points = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
         //获取地图控件引用
         mapView = (MapView) findViewById(R.id.map);
+        btn = (Button) findViewById(R.id.btn);
+        plan = (Button) findViewById(R.id.plan);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mapView.onCreate(savedInstanceState);
 
@@ -49,10 +63,56 @@ public class MainActivity extends AppCompatActivity {
             aMap = mapView.getMap();
         }
 
+
+        aMap.setOnMapClickListener(new AMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Log.e("地图点击", latLng.toString());
+                points.add(new LatLng(latLng.latitude, latLng.longitude));
+
+            }
+        });
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (points.isEmpty()) {
+                    ToastUtil.showShort(MainActivity.this, "请先设置路线");
+                } else {
+                    startMove();
+                }
+            }
+        });
+
+        plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (points.isEmpty() || points.size() < 2) {
+                    ToastUtil.showShort(MainActivity.this, "请先设置路线");
+                } else {
+                    //路线搜索
+                    RouteSearch routeSearch = new RouteSearch(MainActivity.this);
+                    routeSearch.setRouteSearchListener(MainActivity.this);
+                    // fromAndTo包含路径规划的起点和终点，drivingMode表示驾车模式
+                    // 第三个参数表示途经点（最多支持16个），第四个参数表示避让区域（最多支持32个），第五个参数表示避让道路
+
+                    RouteSearch.FromAndTo fromAndTo = new RouteSearch.FromAndTo(
+                            new LatLonPoint(points.get(0).latitude, points.get(0).longitude),
+                            new LatLonPoint(points.get(1).latitude, points.get(1).longitude));
+                    RouteSearch.DriveRouteQuery query = new RouteSearch.DriveRouteQuery(fromAndTo, RouteSearch.DRIVING_SINGLE_DEFAULT, null, null, "");
+                    routeSearch.calculateDriveRouteAsyn(query);
+                }
+
+
+            }
+        });
+
         aMap.showIndoorMap(false);  //不显示室内地图
         aMap.setMaxZoomLevel(14);
         aMap.setMinZoomLevel(12);
-        uiSettings = aMap.getUiSettings();
+        UiSettings uiSettings = aMap.getUiSettings();
         //缩放控件
         uiSettings.setZoomControlsEnabled(false);
 
@@ -61,8 +121,7 @@ public class MainActivity extends AppCompatActivity {
         uiSettings.setZoomGesturesEnabled(true);//缩放手势
         uiSettings.setScrollGesturesEnabled(true);//滑动手势
 
-        setLine();
-        startMove();
+
     }
 
     @Override
@@ -93,10 +152,6 @@ public class MainActivity extends AppCompatActivity {
         mapView.onSaveInstanceState(outState);
     }
 
-    public void setLine() {
-        addPolylineInPlayGround();
-    }
-
     /**
      * 开始移动
      */
@@ -106,9 +161,6 @@ public class MainActivity extends AppCompatActivity {
             ToastUtil.showShort(this, "请先设置路线");
             return;
         }
-
-        // 读取轨迹点
-        List<LatLng> points = readLatLngs();
         // 构建 轨迹的显示区域
         LatLngBounds bounds = new LatLngBounds(points.get(0), points.get(points.size() - 2));
         aMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
@@ -127,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
         // 设置轨迹点
         smoothMarker.setPoints(subList);
         // 设置平滑移动的总时间  单位  秒
-        smoothMarker.setTotalDuration(40);
+        smoothMarker.setTotalDuration(100);
 
         // 设置  自定义的InfoWindow 适配器
         aMap.setInfoWindowAdapter(infoWindowAdapter);
@@ -138,6 +190,7 @@ public class MainActivity extends AppCompatActivity {
         smoothMarker.setMoveListener(new SmoothMoveMarker.MoveListener() {
             @Override
             public void move(final double distance) {
+
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -199,7 +252,7 @@ public class MainActivity extends AppCompatActivity {
             snippet = new TextView(this);
             title.setTextColor(Color.BLACK);
             snippet.setTextColor(Color.BLACK);
-            //infoWindowLayout.setBackgroundResource(R.drawable.infowindow_bg);
+            infoWindowLayout.setBackgroundResource(R.drawable.bg);
 
             infoWindowLayout.addView(title);
             infoWindowLayout.addView(snippet);
@@ -211,108 +264,47 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 添加轨迹线
      */
-    private void addPolylineInPlayGround() {
-        List<LatLng> list = readLatLngs();
-        List<Integer> colorList = new ArrayList<Integer>();
-        List<BitmapDescriptor> bitmapDescriptors = new ArrayList<BitmapDescriptor>();
-
-        int[] colors = new int[]{Color.argb(255, 0, 255, 0), Color.argb(255, 255, 255, 0), Color.argb(255, 255, 0, 0)};
-
-        //用一个数组来存放纹理
-        List<BitmapDescriptor> textureList = new ArrayList<>();
-        textureList.add(BitmapDescriptorFactory.fromResource(R.drawable.custtexture));
-
-        List<Integer> texIndexList = new ArrayList<Integer>();
-        texIndexList.add(0);//对应上面的第0个纹理
-        texIndexList.add(1);
-        texIndexList.add(2);
-
-        Random random = new Random();
-        for (int i = 0; i < list.size(); i++) {
-            colorList.add(colors[random.nextInt(3)]);
-            bitmapDescriptors.add(textureList.get(0));
-
-        }
-
-        mPolyline = aMap.addPolyline(new PolylineOptions().setCustomTexture(BitmapDescriptorFactory.fromResource(R.drawable.custtexture)) //setCustomTextureList(bitmapDescriptors)
-//				.setCustomTextureIndex(texIndexList)
-                .addAll(list)
-                .useGradient(true)
-                .width(18));
-
-        LatLngBounds bounds = new LatLngBounds(list.get(0), list.get(list.size() - 2));
-        aMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+    private void addPolyline() {
+        mPolyline = aMap.addPolyline(
+                new PolylineOptions()
+                        .addAll(points)
+                        .useGradient(true)
+                        .width(8)
+                        .color(R.color.test));
     }
 
-    /**
-     * 读取坐标点
-     *
-     * @return
-     */
-    private List<LatLng> readLatLngs() {
-        List<LatLng> points = new ArrayList<LatLng>();
-        for (int i = 0; i < coords.length; i += 2) {
-            points.add(new LatLng(coords[i + 1], coords[i]));
-        }
-        return points;
+
+    @Override
+    public void onBusRouteSearched(BusRouteResult busRouteResult, int i) {
+        Log.e("公交路线", "" + busRouteResult.toString());
     }
 
-    /**
-     * 坐标点数组数据
-     */
-    private double[] coords = {116.3499049793749, 39.97617053371078,
-            116.34978804908442, 39.97619854213431, 116.349674596623,
-            39.97623045687959, 116.34955525200917, 39.97626931100656,
-            116.34943728748914, 39.976285626595036, 116.34930864705592,
-            39.97628129172198, 116.34918981582413, 39.976260803938594,
-            116.34906721558868, 39.97623535890678, 116.34895185151584,
-            39.976214717128855, 116.34886935936889, 39.976280148755315,
-            116.34873954611332, 39.97628182112874, 116.34860763527448,
-            39.97626038855863, 116.3484658907622, 39.976306080391836,
-            116.34834585430347, 39.976358252119745, 116.34831166130878,
-            39.97645709321835, 116.34827643560175, 39.97655231226543,
-            116.34824186261169, 39.976658372925556, 116.34825080406188,
-            39.9767570732376, 116.34825631960626, 39.976869087779995,
-            116.34822111635201, 39.97698451764595, 116.34822901510276,
-            39.977079745909876, 116.34822234337618, 39.97718701787645,
-            116.34821627457707, 39.97730766147824, 116.34820593515043,
-            39.977417746816776, 116.34821013897107, 39.97753930933358
-            , 116.34821304891533, 39.977652209132174, 116.34820923399242,
-            39.977764016531076, 116.3482045955917, 39.97786190186833,
-            116.34822159449203, 39.977958856930286, 116.3482256370537,
-            39.97807288885813, 116.3482098441266, 39.978170063673524,
-            116.34819564465377, 39.978266951404066, 116.34820541974412,
-            39.978380693859116, 116.34819672351216, 39.97848741209275,
-            116.34816588867105, 39.978593409607825, 116.34818489339459,
-            39.97870216883567, 116.34818473446943, 39.978797222300166,
-            116.34817728972234, 39.978893492422685, 116.34816491505472,
-            39.978997133775266, 116.34815408537773, 39.97911413849568,
-            116.34812908154862, 39.97920553614499, 116.34809495907906,
-            39.979308267469264, 116.34805113358091, 39.97939658036473,
-            116.3480310509613, 39.979491697188685, 116.3480082124968,
-            39.979588529006875, 116.34799530586834, 39.979685789111635,
-            116.34798818413954, 39.979801430587926, 116.3479996420353,
-            39.97990758587515, 116.34798697544538, 39.980000796262615,
-            116.3479912988137, 39.980116318796085, 116.34799204219203,
-            39.98021407403913, 116.34798535084123, 39.980325006125696,
-            116.34797702460183, 39.98042511477518, 116.34796288754136,
-            39.98054129336908, 116.34797509821901, 39.980656820423505,
-            116.34793922017285, 39.98074576792626, 116.34792586413015,
-            39.98085620772756, 116.3478962642899, 39.98098214824056,
-            116.34782449883967, 39.98108306010269, 116.34774758827285,
-            39.98115277119176, 116.34761476652932, 39.98115430642997,
-            116.34749135408349, 39.98114590845294, 116.34734772765582,
-            39.98114337322547, 116.34722082902628, 39.98115066909245,
-            116.34708205250223, 39.98114532232906, 116.346963237696,
-            39.98112245161927, 116.34681500222743, 39.981136637759604,
-            116.34669622104072, 39.981146248090866, 116.34658043260109,
-            39.98112495260716, 116.34643721418927, 39.9811107163792,
-            116.34631638374302, 39.981085081075676, 116.34614782996252,
-            39.98108046779486, 116.3460256053666, 39.981049089345206,
-            116.34588814050122, 39.98104839362087, 116.34575119741586,
-            39.9810544889668, 116.34562885420186, 39.981040940565734,
-            116.34549232235582, 39.98105271658809, 116.34537348820508,
-            39.981052294975264, 116.3453513775533, 39.980956549928244
-    };
+
+    @Override
+    public void onDriveRouteSearched(DriveRouteResult result, int errorCode) {
+        Log.e("驾车路线", "" + result.getPaths().size());
+
+        points.clear();
+        DrivePath drivePath = result.getPaths().get(0);
+        List<DriveStep> driveSteps = drivePath.getSteps();
+        for (DriveStep driveStep : driveSteps) {
+            List<LatLonPoint> latLonPoints = driveStep.getPolyline();
+            for (LatLonPoint latLonPoint : latLonPoints) {
+                points.add(new LatLng(latLonPoint.getLatitude(), latLonPoint.getLongitude()));
+            }
+        }
+
+        addPolyline();
+    }
+
+    @Override
+    public void onWalkRouteSearched(WalkRouteResult walkRouteResult, int i) {
+        Log.e("走路路线", "" + walkRouteResult.toString());
+    }
+
+    @Override
+    public void onRideRouteSearched(RideRouteResult rideRouteResult, int i) {
+        Log.e("骑车路线", "" + rideRouteResult.toString());
+    }
 }
 
